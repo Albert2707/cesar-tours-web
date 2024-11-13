787f1a8b58b2dee1b45fac26bc5ff2f0d7bd881d
Albert2707 <albertjohan2707@gmail.com>
2024-11-13 14:01:00 -0400

    Animations to hero sections

--- src/components/heroSection/HeroSection.tsx
+++ src/components/heroSection/HeroSection.tsx
@@ -1,15 +1,23 @@
+import { useRef } from "react";
 import useTranslate from "../../hooks/Translate";
 import "./HeroSection.scss";
+import { useScroll, useTransform, motion } from "framer-motion";
 const HeroSection = () => {
+  const wrapperRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: wrapperRef,
+    offset: ["start start", "end start"],
+  });
+  const y = useTransform(scrollYProgress, [0, 1], ["0%", "50%"]);
   const { translate } = useTranslate();
   return (
     <section className="hero-section" id="home">
-      <div className="wrapper">
+      <motion.div className="wrapper" ref={wrapperRef} style={{ y }}>
         <h3>{translate("yourDestinationIsOurDestination")} </h3>
         <h1>{translate("cesarDestination")}</h1>
         <p>{translate("travelFastAndSafe")}</p>
         <a href="#booking">{translate("bookNow")}</a>
-      </div>
+      </motion.div>
     </section>
   );
 };
