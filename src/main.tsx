dec96bc8c416282f8f9583eeec3f0d568c230077
Albert2707 <albertjohan2707@gmail.com>
2024-11-17 17:42:51 -0400

    adding query client

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.scss'
 import App from './App.tsx'
 import { IdiomContextProvider } from './context/idiomContext.tsx'
-
+import { QueryClient, QueryClientProvider} from 'react-query'
+const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <IdiomContextProvider>
+    <QueryClientProvider client={queryClient}>
     <App />
+    </QueryClientProvider>
     </IdiomContextProvider>
   </StrictMode>
 )
