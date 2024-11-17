64ff817ce3e48d5cd41fae611fcba0544104f04d
Albert2707 <albertjohan2707@gmail.com>
2024-11-17 17:43:21 -0400

    moving to a directory

--- src/utils/request.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const apiKey = import.meta.env.VITE_API_KEY;
-const api = import.meta.env.VITE_CESAR_API;
-export const request = axios.create({
-  baseURL:api+"/api/",
-  headers: {
-    resendApiKey: apiKey,
-  },
-});
