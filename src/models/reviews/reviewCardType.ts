78914351c1201da0467ffaf9728f35a770653d29
Albert2707 <albertjohan2707@gmail.com>
2024-11-17 17:47:51 -0400

    reviewCard type

---
+++ src/models/reviews/reviewCardType.ts
@@ -0,0 +1,12 @@
+export interface ReviewsCardType {
+    img: string;
+    name: string;
+    date: string;
+    rating: number;
+    comment: string;
+    desc: string;
+  }
+  export interface Geo{
+    lat?: number
+    lng?: number
+  }
