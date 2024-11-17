13514ae794592bc9d7483f254ef124641ecb2f42
Albert2707 <albertjohan2707@gmail.com>
2024-11-17 17:46:31 -0400

    fetching data from server

--- src/components/booking/components/steps/Vehicle.tsx
+++ src/components/booking/components/steps/Vehicle.tsx
@@ -1,21 +1,25 @@
 import { motion } from "framer-motion";
 import "./Vehicle.scss";
+import { format} from 'date-fns'
 import { FC } from "react";
-import useTranslate from "../../../../hooks/Translate";
+import useTranslate from "../../../../hooks/translations/Translate";
+import { useQuery } from "react-query";
+import { request } from "../../../../utils/api/request";
+import { VehicleModel } from "../../../../models/booking/vehicle";
+import { useBookingStore } from "../../../../hooks/booking/useBookingStore";
+import { useIdiom } from "../../../../context/idiomContext";
+import { IdiomTypes } from "../../../../context/idiomTypes";
 
 interface Props {
   setStep: React.Dispatch<React.SetStateAction<number>>;
 }
 
 const Vehicle: FC<Props> = ({ setStep }) => {
+  const {idiom} = useIdiom() as IdiomTypes
+  const { passengerNo, bagsNo, departureHour, departureDate } =
+    useBookingStore();
   const { translate } = useTranslate();
 
-  const vehicles = [
-    { img: "images/suburban.png", name: "Tahoe Suburban", price: "350,50" },
-    { img: "images/coaster.png", name: "Toyota Coaster", price: "300,00" },
-    { img: "images/hiace.png", name: "Toyota Hiace", price: "250,61" },
-  ];
-  
   const variants = {
     initial: {
       x: 100,
@@ -30,6 +34,20 @@ const Vehicle: FC<Props> = ({ setStep }) => {
       },
     },
   };
+
+  const { data, isLoading } = useQuery(
+    "vehicles",
+    async () => {
+      return request
+        .get(`vehicle/getVehicles/${passengerNo}/${bagsNo}`)
+        .then((res) => {
+          return res.data;
+        })
+        .catch((err) => err);
+    }
+    // { refetchOnWindowFocus: false }
+  );
+
   return (
     <div className="select-vehicle">
       <div className="booking-info">
@@ -45,7 +63,7 @@ const Vehicle: FC<Props> = ({ setStep }) => {
           </div>
           <div className="collection-time">
             <span>Collection Time</span>
-            <span>24/11/2024 - 03:15</span>
+            <span>{format(departureDate!,idiom === "es" ? "dd/MM/yyyy" : "MM/dd/yyy")} - {departureHour}</span>
           </div>
           <div className="distance">
             <span>Distance</span>
@@ -81,71 +99,71 @@ const Vehicle: FC<Props> = ({ setStep }) => {
         animate="animate"
         variants={variants}
       >
-        {vehicles.map((e) => (
-          <motion.div
-            key={crypto.randomUUID()}
-            variants={variants}
-            className="vehicle"
-          >
-            <div className="vehicle-img">
-              <img
-                src={e.img}
-                alt="Tahoe Suburban"
-                width={200}
-                height={1200}
-                loading="lazy"
-              />
-            </div>
+        {isLoading
+          ? "Loading..."
+          : data.map((e: VehicleModel) => (
+              <div key={crypto.randomUUID()} className="vehicle">
+                <div className="vehicle-img">
+                  <img
+                    src={e.img_url}
+                    alt="Tahoe Suburban"
+                    width={200}
+                    height={1200}
+                    loading="lazy"
+                  />
+                </div>
 
-            <div className="features">
-              <div className="vehicle-info">
-                <span>{e.name}</span>
-                <span className="price">$&nbsp;{e.price}</span>
-              </div>
-
-              <div className="book-now">
-                <div className="more">
-                  <div className="people">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="size-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
-                      />
-                    </svg>
-                    <span>8</span>
+                <div className="features">
+                  <div className="vehicle-info">
+                    <span>
+                      {e.make} {e.model}
+                    </span>
+                    <span className="price">$&nbsp;{e.price_per_km}</span>
                   </div>
-                  <div className="luggage">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="size-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z"
-                      />
-                    </svg>
 
-                    <span>20</span>
+                  <div className="book-now">
+                    <div className="more">
+                      <div className="people">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="size-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                          />
+                        </svg>
+                        <span>{e.capacity}</span>
+                      </div>
+                      <div className="luggage">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="size-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z"
+                          />
+                        </svg>
+
+                        <span>{e.luggage_capacity}</span>
+                      </div>
+                    </div>
+                    <button>BOOK NOW</button>
                   </div>
                 </div>
-                <button>BOOK NOW</button>
               </div>
-            </div>
-          </motion.div>
-        ))}
+            ))}
       </motion.div>
     </div>
   );
