7d2552b5d3b9385a9f2a0a7fb6776be579ac51a2
Albert <albertjohan2707@gmail.com>
2024-12-01 20:57:44 -0400

    Context
    Adding authContext to provide authentication

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import './index.scss'
 import App from './App.tsx'
 import { IdiomContextProvider } from './context/idiomContext.tsx'
 import { QueryClient, QueryClientProvider} from 'react-query'
+import AuthContextProvider from './context/authContext.tsx'
 const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <AuthContextProvider>
     <IdiomContextProvider>
     <QueryClientProvider client={queryClient}>
     <App />
     </QueryClientProvider>
     </IdiomContextProvider>
+    </AuthContextProvider>
   </StrictMode>
 )
