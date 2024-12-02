6f7f336617be9ac8a1c44f2d7dcd3453c205bdcd
Albert <albertjohan2707@gmail.com>
2024-12-01 23:17:14 -0400

    adding credentials to axios

--- src/utils/api/request.ts
+++ src/utils/api/request.ts
@@ -2,8 +2,9 @@ import axios from "axios";
 const apiKey = import.meta.env.VITE_API_KEY;
 // const api = import.meta.env.VITE_CESAR_API;
 export const request = axios.create({
-  baseURL:"http://localhost:8000/api/",
+  baseURL: "http://localhost:8000/api/",
   headers: {
     resendApiKey: apiKey,
   },
+  withCredentials: true,
 });
