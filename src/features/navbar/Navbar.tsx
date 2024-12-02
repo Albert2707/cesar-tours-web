e60f56f78484df89a89311cba3681b1029fcfb52
Albert2707 <albertjohan2707@gmail.com>
2024-12-02 08:32:01 -0400

    fix: duplicate lines, Select component

--- src/features/navbar/Navbar.tsx
+++ src/features/navbar/Navbar.tsx
@@ -1,37 +1,19 @@
 import { useState } from "react";
-import { useIdiom } from "../../context/idiomContext";
-import { IdiomTypes } from "../../context/idiomTypes";
 import MobileMenu from "./components/mobileMenu/MobileMenu";
 import "./Navbar.scss";
 import { AnimatePresence } from "framer-motion";
-import Select from "react-select";
 import { NavLink } from "react-router-dom";
 import useTranslate from "../../shared/hooks/translations/Translate";
+import SelectIdiom from "../../shared/components/selectIdiom/SelectIdiom";
 
 const Navbar = () => {
-  const { setLanguage, idiom } = useIdiom() as IdiomTypes;
   const { translate } = useTranslate();
   const [isOpen, setIsOpen] = useState(false);
-  const options = [
-    { value: "es", label: translate("Spanish"), labelOnSelect: "es" },
-    { value: "en", label: translate("English"), labelOnSelect: "us" },
-  ];
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
-const formatLabel = (option:any) => (
-  <div
-    style={{
-      display: "flex",
-      alignItems: "center",
-      gap: "5px",
-      justifyContent: "center",
-    }}
-  >
-    <span>{option.label}</span>
-  </div>
-)
+
 
   return (
     <nav className="nav">
@@ -70,145 +52,12 @@ const formatLabel = (option:any) => (
               <a href="#contact">{translate("contactUs")}</a>
             </li>
             <li>
-              <Select
-                options={options}
-                styles={{
-                  control: (baseStyles) => ({
-                    ...baseStyles,
-                    backgroundColor: "transparent",
-                    borderRadius: "10px",
-                    height: "40px",
-                    display: "flex",
-                    width: "100px",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    fontSize: "12px",
-                    borderColor: "gray",
-                    boxShadow: "none",
-                    "&:hover": {
-                      borderColor: "#f2f2f2",
-                    },
-                  }),
-                  menuPortal: (base) => ({ ...base, zIndex: 9999 }),
-                  menu: (baseStyles) => ({
-                    ...baseStyles,
-                    borderRadius: "10px",
-                    fontSize: "12px",
-                    backgroundColor: "#f2f2f2",
-                    fontWeight: 600,
-                    // zIndex: 9999, // Asegura que el menú esté al frente
-                  }),
-                  option: (baseStyles, state) => ({
-                    ...baseStyles,
-                    backgroundColor: state.isFocused
-                      ? "rgba(242, 75, 15, 0.1)"
-                      : "transparent",
-                    color: state.isFocused ? "orange" : "inherit",
-                    "&:active": {
-                      backgroundColor: "rgba(242, 75, 15, 0.1)",
-                    },
-                  }),
-                  indicatorSeparator: (styles) => ({
-                    ...styles,
-                    display: "none",
-                  }),
-                  dropdownIndicator: (styles) => ({
-                    ...styles,
-                    display: "none",
-                  }),
-                  valueContainer: (baseStyles) => ({
-                    ...baseStyles,
-                    padding: "0",
-                    margin: "0 8px",
-                  }),
-                  singleValue: (baseStyles) => ({
-                    ...baseStyles,
-                    margin: "0",
-                    color: "#f2f2f2",
-                    paddingLeft: "0",
-                  }),
-                }}
-                formatOptionLabel={formatLabel}
-                placeholder={translate("travelType")}
-                isSearchable={false}
-                value={options.find((option) => option.value === idiom)}
-                closeMenuOnScroll={true}
-                menuPortalTarget={document.body}
-                menuPlacement="auto"
-                menuPosition="fixed"
-                onChange={(e) => setLanguage(e?.value as "es" | "en")}
-              />
+              <SelectIdiom/>
             </li>
           </ul>
           <div className="mobile-menu">
-            <Select
-              options={options}
-              styles={{
-                control: (baseStyles) => ({
-                  ...baseStyles,
-                  backgroundColor: "transparent",
-                  borderRadius: "10px",
-                  height: "40px",
-                  display: "flex",
-                  width: "100px",
-                  justifyContent: "center",
-                  transition: "all 0.3s ease-in-out",
-                  alignItems: "center",
-                  fontSize: "12px",
-                  borderColor: "gray",
-                  boxShadow: "none",
-                  "&:hover": {
-                    borderColor: "#f2f2f2",
-                  },
-                }),
-                menuPortal: (base) => ({ ...base, zIndex: 9999 }),
-                menu: (baseStyles) => ({
-                  ...baseStyles,
-                  borderRadius: "10px",
-                  fontSize: "12px",
-                  backgroundColor: "#f2f2f2",
-                  fontWeight: 600,
-                }),
-                option: (baseStyles, state) => ({
-                  ...baseStyles,
-                  backgroundColor: state.isFocused
-                    ? "rgba(242, 75, 15, 0.1)"
-                    : "transparent",
-                  color: state.isFocused ? "orange" : "inherit",
-                  "&:active": {
-                    backgroundColor: "rgba(242, 75, 15, 0.1)",
-                  },
-                }),
-                indicatorSeparator: (styles) => ({
-                  ...styles,
-                  display: "none",
-                }),
-                dropdownIndicator: (styles) => ({
-                  ...styles,
-                  display: "none",
-                }),
-                valueContainer: (baseStyles) => ({
-                  ...baseStyles,
-                  padding: "0",
-                  margin: "0 8px",
-                }),
-                singleValue: (baseStyles) => ({
-                  ...baseStyles,
-                  margin: "0",
-                  color: "#f2f2f2",
-                  paddingLeft: "0",
-                }),
-              }}
-              formatOptionLabel={formatLabel}
-              placeholder={translate("travelType")}
-              isSearchable={false}
-              value={options.find((option) => option.value === idiom)}
-              closeMenuOnScroll={true}
-              menuPortalTarget={document.body}
-              menuPlacement="auto"
-              menuPosition="fixed"
-              onChange={(e) => setLanguage(e?.value as "es" | "en")}
-            />
+          <SelectIdiom/>
+
             <button aria-label="menu-button" onClick={toggleMenu}>
               {isOpen ? (
                 <svg
