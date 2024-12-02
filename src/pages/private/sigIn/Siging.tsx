4606f80ee33b5aeccd7785a79ae4417426c8a04b
Albert <albertjohan2707@gmail.com>
2024-12-01 23:21:56 -0400

    feat: sigin page

---
+++ src/pages/private/sigIn/Siging.tsx
@@ -0,0 +1,11 @@
+import Login from "../../../features/login/Login"
+import "./Siging.scss"
+const Siging = () => {
+    return (
+        <div className="login">
+            <Login />
+        </div>
+    )
+}
+
+export default Siging
