4b477bdc47d76af30d6c776fb1ab8ecc7a74df39
Albert <albertjohan2707@gmail.com>
2024-12-01 23:20:43 -0400

    moving to features folder

---
+++ src/features/experience/Experience.tsx
@@ -0,0 +1,85 @@
+import "./Experience.scss"
+import useTranslate from '../../shared/hooks/translations/Translate';
+
+const Experience = () => {
+    const { translate } = useTranslate();
+    return (
+        <div className="experience-section">
+            <div className="wrapper">
+                <h1>
+                    {translate("experience_speaks")}
+                </h1>
+                <div className="experience-pictures">
+                    <div className="picture">
+                        <img
+                            src="images/view.jpg"
+                            alt="" loading="lazy"
+                        />
+                    </div>
+                    {/* <div className="picture">
+
+                        <img
+                            src="https://images.pexels.com/photos/20734769/pexels-photo-20734769/free-photo-of-hombre-vacaciones-arena-mujer.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1/"
+                            alt="" loading="lazy"
+                        />
+                    </div> */}
+                    <div className="picture">
+
+                        <img
+                            src="images/viva.jpg"
+                            alt="" loading="lazy"
+                        />
+                    </div>
+                    <div className="picture">
+
+
+                        <img
+                            src="images/ocean.jpg"
+                            alt="" loading="lazy"
+                        />
+                    </div>
+                    <div className="picture">
+
+                        <img
+                            src="images/beach.jpg"
+                            alt="" loading="lazy"
+                        />
+                    </div>
+                    <div className="picture">
+
+                        <img
+                            src="images/beach2.jpg"
+                            alt="" loading="lazy"
+                        />
+                    </div>
+                    <div className="picture">
+
+                        <img
+                            src="images/travel.jpg"
+                            alt="" loading="lazy"
+                        />
+                    </div>
+                </div>
+                <h1>
+                    {translate("some_videos")}
+                </h1>
+                <div className="experience-videos">
+                    <div className="video">
+
+                        <video controls={false} autoPlay muted loop>
+                            <source src="videos/video1.mp4" type="video/mp4" />
+                        </video>
+                    </div>
+                    <div className="video">
+
+                        <video controls={false} autoPlay muted loop>
+                            <source src="videos/video2.mp4" type="video/mp4" />
+                        </video>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Experience
---
+++ src/features/heroSection/HeroSection.tsx
@@ -0,0 +1,25 @@
+import { useRef } from "react";
+import useTranslate from "../../shared/hooks/translations/Translate";
+import "./HeroSection.scss";
+import { useScroll, useTransform, motion } from "framer-motion";
+const HeroSection = () => {
+  const wrapperRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: wrapperRef,
+    offset: ["start start", "end start"],
+  });
+  const y = useTransform(scrollYProgress, [0, 1], ["0%", "30%"]);
+  const { translate } = useTranslate();
+  return (
+    <section className="hero-section" id="home">
+      <motion.div initial={{ opacity: 0, x: -100 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 0.3 }} className="wrapper" ref={wrapperRef} style={{ y }}>
+        <h3>{translate("yourDestinationIsOurDestination")} </h3>
+        <h1>{translate("cesarDestination")}</h1>
+        <p>{translate("travelFastAndSafe")}</p>
+        <a href="#booking">{translate("bookNow")}</a>
+      </motion.div>
+    </section>
+  );
+};
+
+export default HeroSection;
