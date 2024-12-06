c8b8f856b5bf43d02a820cee3023850ce3ad4601
Albert <albertjohan2707@gmail.com>
2024-12-06 12:07:32 -0400

    feat: footer as part of the Main layout

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,12 +1,14 @@
 import { lazy } from 'react'
 const Navbar = lazy(() => import('../features/navbar/Navbar'))
 import { Outlet } from 'react-router-dom'
+import Footer from '../features/footer/Footer'
 
 const MainLayout = () => {
   return (
     <>
     <Navbar />
     <Outlet />
+    <Footer/>
   </>  )
 }
 
