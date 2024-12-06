800af20d1e7add621eefb8ee5519b6b00a988b25
Albert <albertjohan2707@gmail.com>
2024-12-06 12:06:37 -0400

    feat: adding vehicle to BookInfo type

--- src/shared/hooks/booking/useBookingStore.ts
+++ src/shared/hooks/booking/useBookingStore.ts
@@ -1,28 +1,31 @@
 import { create } from "zustand";
+import { VehicleModel } from "../../../models/booking/vehicle";
 
-type Inputs = {
+type BookInfo = {
   trip_type?: number;
   passengerNo?: number;
   bagsNo?: number;
   departureHour?: string;
   departureDate: Date;
+  vehicle?: VehicleModel
 };
 
-const values:Inputs ={
-    passengerNo: 1,
-    bagsNo: 0,
-    trip_type: 1, 
-    departureDate: new Date(),
+const values: BookInfo = {
+  passengerNo: 1,
+  bagsNo: 0,
+  trip_type: 1,
+  departureDate: new Date(),
 }
 type Actions = {
   setTripType: (trip_type: number) => void;
   setDepartureDate: (date: Date) => void;
   setDepartureHour: (hour: string) => void;
   setNoPassenger: (passengers: number) => void;
+  setVehicle: (vehicle: VehicleModel) => void;
   setBagsNo: (bagsNo: number) => void;
 };
-export const useBookingStore = create<Inputs & Actions>()((set) => ({
-...values,
+export const useBookingStore = create<BookInfo & Actions>()((set) => ({
+  ...values,
   setTripType: (trip_type) => set(() => ({ trip_type })),
   setDepartureDate: (date) =>
     set(() => {
@@ -40,4 +43,5 @@ export const useBookingStore = create<Inputs & Actions>()((set) => ({
     set(() => {
       return { bagsNo };
     }),
+  setVehicle: (vehicle) => set(() => { return { vehicle } })
 }));
