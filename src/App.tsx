e0726c45046acd4942a304c7d02ae865ddab3f86
Albert2707 <albertjohan2707@gmail.com>
2024-12-11 07:59:01 -0400

    fix: lazy components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { lazy, Suspense, useEffect } from "react";
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { lazy, Suspense } from "react";
 const NotFound = lazy(() => import("./pages/public/notFound/NotFound"));
 const Login = lazy(() => import("./pages/private/sigIn/Siging"));
-const PrivateRoutes = lazy(() => import("./shared/hooks/privateRoutes/PrivateRoutes"));
+const PrivateRoutes = lazy(
+  () => import("./shared/hooks/privateRoutes/PrivateRoutes")
+);
 const Dashboard = lazy(() => import("./pages/private/dashboard/Dashboard"));
 const AdminLayout = lazy(() => import("./layout/privateLayout/AdminLayout"));
-import { HelmetProvider } from 'react-helmet-async';
-import Home from "./pages/public/home/Home";
-import FallBack from "./pages/public/fallBack/FallBack";
-import MainLayout from "./layout/MainLayout";
-import Checkout from "./pages/public/checkout/Checkout";
+const Home = lazy(() => import("./pages/public/home/Home"));
+import { HelmetProvider } from "react-helmet-async";
+const FallBack = lazy(() => import("./pages/public/fallBack/FallBack"));
+const MainLayout = lazy(() => import("./layout/MainLayout"));
+const Checkout = lazy(() => import("./pages/public/checkout/Checkout"));
+const Confirm = lazy(() => import("./pages/public/confirm/Confirm"));
+import Vehicles from "./pages/private/vehicles/Vehicles";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 function App() {
-
   const router = createBrowserRouter([
     {
       path: "/",
       element: <MainLayout />,
       children: [
         {
-          path: '/',
-          element: <Home />
+          path: "/",
+          element: <Home />,
         },
         {
-          path: '/checkout',
-          element: <Checkout />
-        }
-      ]
+          path: "/checkout",
+          element: <Checkout />,
+        },
+        {
+          path: "/order/confirmation",
+          element: <Confirm />,
+        },
+      ],
     },
     {
       path: "/login",
@@ -37,34 +40,53 @@ function App() {
     },
     {
       path: "/admin",
-      element: <PrivateRoutes>
-        <AdminLayout />
-      </PrivateRoutes>,
+      element: (
+        <PrivateRoutes>
+          <AdminLayout />
+        </PrivateRoutes>
+      ),
       children: [
         {
-          path: '/admin/orders',
-          element: <Dashboard />
-        }
-      ]
+          path: "/admin/orders",
+          element: <Dashboard />,
+        },
+        {
+          path: "/admin/vehicles",
+          element: <Vehicles />,
+        },
+      ],
     },
     {
       path: "*",
       element: <NotFound />,
-    }
+    },
   ]);
-  useEffect(() => {
-    const { hash } = window.location;
-    // Solo ejecuta replaceState si hay un hash en la URL.
-    if (hash) {
-      history.replaceState(null, "", window.location.pathname);
-    }
-    window.scrollTo(0, 0);
-  }, []);
+
   const helmetContext = {};
+
   return (
     <HelmetProvider context={helmetContext}>
       <Suspense fallback={<FallBack />}>
         <RouterProvider router={router} />
+        {/* <Routes>
+            <Route path="/" element={<MainLayout />}>
+              <Route index element={<Home />} />
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="/order/confirmation" element={<Confirm />} />
+            </Route>
+            <Route path="login" element={<Login />} />
+            <Route
+              element={
+                <PrivateRoutes>
+                  <AdminLayout />
+                </PrivateRoutes>
+              }
+            >
+              <Route path="/admin/orders" element={<Dashboard />} />
+              <Route path="/admin/vehicles" element={<Vehicles />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes> */}
       </Suspense>
     </HelmetProvider>
   );
