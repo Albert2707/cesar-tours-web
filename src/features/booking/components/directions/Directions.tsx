d26ba4852918c9bfc32d17c160f619cae987f959
Albert2707 <albertjohan2707@gmail.com>
2024-12-11 08:07:14 -0400

    feat: adding origin and destination state

--- src/features/booking/components/directions/Directions.tsx
+++ src/features/booking/components/directions/Directions.tsx
@@ -3,6 +3,7 @@ import { FC, useEffect, useState } from "react";
 import useTranslate from "../../../../shared/hooks/translations/Translate";
 import { useIdiom } from "../../../../context/idiomContext";
 import { IdiomTypes } from "../../../../context/idiomTypes";
+import { useBookingStore } from "../../../../shared/hooks/booking/useBookingStore";
 
 type DirectionsProps = {
   origin: string;
@@ -12,19 +13,28 @@ type DirectionsProps = {
 export const Directions: FC<DirectionsProps> = ({ origin, destination }) => {
   const { idiom } = useIdiom() as IdiomTypes;
   const { translate } = useTranslate();
-
+  const {
+    setDuration,
+    setDistance,
+    duration,
+    distance,
+    setDestination,
+    setOrigin,
+  } = useBookingStore();
   const map = useMap();
-  const routesLibrary = useMapsLibrary('routes');
-  const [directionsService, setDirectionsService] = useState<google.maps.DirectionsService>();
-  const [directionsRenderer, setDirectionsRenderer] = useState<google.maps.DirectionsRenderer>();
+  const routesLibrary = useMapsLibrary("routes");
+  const [directionsService, setDirectionsService] =
+    useState<google.maps.DirectionsService>();
+  const [directionsRenderer, setDirectionsRenderer] =
+    useState<google.maps.DirectionsRenderer>();
   const [routes, setRoutes] = useState<google.maps.DirectionsRoute[]>([]);
-  const routeIndex=0;
-  const selected = routes[routeIndex];
-  const leg = selected?.legs[0];
+  const routeIndex = 0;
 
   function traducirDuracion(str: string) {
     if (idiom === "es") {
-      return str.replace(/(hours|hour)/, "horas").replace(/(mins|min)/, "minutos");
+      return str
+        .replace(/(hours|hour)/, "horas")
+        .replace(/(mins|min)/, "minutos");
     }
     return str;
   }
@@ -38,22 +48,38 @@ export const Directions: FC<DirectionsProps> = ({ origin, destination }) => {
 
   // Use directions service and update renderer with new directions
   useEffect(() => {
-    if (!directionsService || !directionsRenderer || !origin || !destination) return;
+    if (!directionsService || !directionsRenderer || !origin || !destination)
+      return;
     // Clear previous route
     directionsRenderer.setMap(null);
     // Request new directions
-    directionsService.route({
-      origin,
-      destination,
-      travelMode: google.maps.TravelMode.DRIVING,
-      provideRouteAlternatives: true
-    })
-    .then(response => {
-      directionsRenderer.setMap(map); // Attach renderer to map
-      directionsRenderer.setDirections(response);
-      setRoutes(response.routes);
-    })
-    .catch(error => console.error("Error fetching directions:", error));
+    directionsService
+      .route({
+        origin,
+        destination,
+        travelMode: google.maps.TravelMode.DRIVING,
+        provideRouteAlternatives: true,
+        region: "DO",
+      })
+      .then((response) => {
+        directionsRenderer.setMap(map); // Attach renderer to map
+        directionsRenderer.setDirections(response);
+        const { routes } = response;
+        if (routes) {
+          const tripInfo = routes[0]?.legs[0];
+          console.log(tripInfo);
+          setOrigin(tripInfo?.start_address);
+          setDestination(tripInfo?.end_address);
+          setDuration({text:tripInfo?.duration?.text as string,
+            value:tripInfo?.duration?.value as number
+          });
+          setDistance({text:tripInfo?.distance?.text as string,
+            value:tripInfo?.distance?.value as number
+          });
+        }
+        setRoutes(response.routes);
+      })
+      .catch((error) => console.error("Error fetching directions:", error));
 
     return () => directionsRenderer.setMap(null); // Clean up on unmount
   }, [directionsService, directionsRenderer, origin, destination, map]);
@@ -64,20 +90,15 @@ export const Directions: FC<DirectionsProps> = ({ origin, destination }) => {
     }
   }, [routeIndex, directionsRenderer, routes.length]);
 
-
   return (
     <div className="location-info">
       <div className="distance">
         <span>{translate("distance")}</span>
-        {
- <span> {leg?.distance?.text ?? "---"} </span>
-        }
+        {<span> {distance?.text ?? "---"} </span>}
       </div>
       <div className="time">
         <span>{translate("time")}</span>
-        {
-          <span>{leg?.duration?.text ? traducirDuracion(leg.duration.text) : "---"}</span>
-        }
+        {<span>{duration ? traducirDuracion(duration.text) : "---"}</span>}
       </div>
     </div>
   );
