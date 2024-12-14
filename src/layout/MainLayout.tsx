0296c2de0694fb1070d84a6e75adc162c37b83ac
Albert <albertjohan2707@gmail.com>
2024-12-14 09:16:54 -0400

    refactor: change relative path to path aliases

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,7 +1,7 @@
 import { lazy } from "react";
 import { Outlet} from "react-router-dom";
-const Navbar = lazy(() => import("../features/navbar/Navbar"));
-const Footer = lazy(() => import("../features/footer/Footer"));
+const Navbar = lazy(() => import("@/features/navbar/Navbar"));
+const Footer = lazy(() => import("@/features/footer/Footer"));
 const MainLayout = () => {
   return (
     <div id="main">
