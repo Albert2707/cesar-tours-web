445f2d1e4999029937c93eb6296d2ac94bb69a13
Albert <albertjohan2707@gmail.com>
2024-12-16 08:42:17 -0400

    feat: scroll to the target when hash change

--- src/pages/public/home/Home.tsx
+++ src/pages/public/home/Home.tsx
@@ -1,21 +1,31 @@
-import React from "react";
-import { Seo } from "../../../shared/components/seo/SEO";
+import React, { useEffect } from "react";
+import { Seo } from "@/shared/components/seo/SEO";
+import { useLocation } from "react-router-dom";
 
 const HeroSection = React.lazy(
-  () => import("../../../features/heroSection/HeroSection")
+  () => import("@/features/heroSection/HeroSection")
 );
-const Reviews = React.lazy(() => import("../../../features/reviews/Reviews"));
-const Booking = React.lazy(() => import("../../../features/booking/Booking"));
+const Reviews = React.lazy(() => import("@/features/reviews/Reviews"));
+const Booking = React.lazy(() => import("@/features/booking/Booking"));
 const Experience = React.lazy(
-  () => import("../../../features/experience/Experience")
+  () => import("@/features/experience/Experience")
 );
-const AboutUs = React.lazy(() => import("../../../features/aboutUs/AboutUs"));
-const Contact = React.lazy(() => import("../../../features/contact/Contact"));
+const AboutUs = React.lazy(() => import("@/features/aboutUs/AboutUs"));
+const Contact = React.lazy(() => import("@/features/contact/Contact"));
 const WhatsApp = React.lazy(
-  () => import("../../../shared/components/whatsApp/WhatsApp")
+  () => import("@/shared/components/whatsApp/WhatsApp")
 );
-
 const Home = () => {
+  const {hash} = useLocation();
+  useEffect(() => {
+    if(hash){
+      const target = document.getElementById(hash.replace("#",""));
+      if (target) {
+        target.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+
+  }, [hash]);
   return (
     <>
       <Seo
