820361d2e8bdcc1f3f9c26cf3eb14f26a9b8b08e
Albert <albertjohan2707@gmail.com>
2024-12-16 08:47:40 -0400

    feat: format hours and adding styles

--- src/pages/private/orderDetail/OrderDetail.tsx
+++ src/pages/private/orderDetail/OrderDetail.tsx
@@ -12,6 +12,7 @@ import { VITE_CESAR_API } from "@/config/config";
 import { useEffect, useState } from "react";
 import { customToast } from "@/utils/functions/customToast";
 import { Toaster } from "react-hot-toast";
+import { formatHour } from "@/utils/functions/formatHour";
 
 const OrderDetail = () => {
   const { id } = useParams();
@@ -26,7 +27,6 @@ const OrderDetail = () => {
       return res.data.order;
     },
   });
-
   const updateState = useMutation({
     mutationFn: async (state: number) => {
       await request.put(`order/updateOrderStatus/${id}`, { status: state })
@@ -38,7 +38,6 @@ const OrderDetail = () => {
       client.invalidateQueries({
         queryKey: ["orders"],
       })
-      customToast("success", "Orden actualizada con exito")
 
     },
     onError: () => {
@@ -100,83 +99,149 @@ const OrderDetail = () => {
             </div>
             <div>{data.vehicle.brand}</div>
             <div>{data.vehicle.model}</div>
+            <div>{moneyFormant(data.vehicle.price_per_km)}</div>
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                />
+              </svg>
+              {data.vehicle.capacity}</div>
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z"
+                />
+              </svg>
+              {data.vehicle.luggage_capacity}</div>
           </div>
-          <div className="order_header">
+          <div className="order_container">
+            <div className="left">
+              <div className="order_header">
 
-            <h3>Detalle de la orden</h3>
-            <Select
-              options={options}
-              value={options.find(e => e.value == data.status)}
-              styles={{
-                control: (baseStyles) => {
-                  const backgroundColor = colorBack[selected?.value]
-                  return {
+                <h3>Detalle de la orden</h3>
+                <Select
+                  options={options}
+                  value={options.find(e => e.value == data.status)}
+                  styles={{
+                    control: (baseStyles) => {
+                      const backgroundColor = colorBack[selected?.value]
+                      return {
 
-                    ...baseStyles,
-                    backgroundColor,
-                    borderRadius: "50px",
-                    height: "30px",
-                    display: "flex",
-                    width: "max-content",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    borderColor: colorMap[selected?.value],
-                    boxShadow: "none",
-                    "&:hover": { borderColor: colorBack[selected?.value] },
-                  }
-                },
-                menuPortal: (base) => ({ ...base, zIndex: 9999 }),
-                menu: (baseStyles) => ({
-                  ...baseStyles,
-                  borderRadius: "10px",
-                  display: "flex",
-                  width: "max-content",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  backgroundColor: "#f2f2f2",
-                  fontWeight: 600,
-                }),
-                option: (baseStyles, state) => {
-                  const color = colorMap[state.data.value]
-                  return {
-                    ...baseStyles,
-                    backgroundColor: "transparent",
-                    color,
-                  };
-                },
-                indicatorSeparator: (styles) => ({ ...styles, display: "none" }),
-                dropdownIndicator: (styles) => ({ ...styles, display: "none" }),
-                valueContainer: (baseStyles) => ({ ...baseStyles, padding: "0", margin: "0 8px" }),
-                singleValue: (baseStyles, state) => {
-                  const color = colorMap[state.data.value]
-                  return {
-                    ...baseStyles,
-                    color,
-                    paddingLeft: "0",
-                  };
-                },
+                        ...baseStyles,
+                        backgroundColor,
+                        borderRadius: "50px",
+                        height: "30px",
+                        display: "flex",
+                        width: "max-content",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        borderColor: colorMap[selected?.value],
+                        boxShadow: "none",
+                        "&:hover": { borderColor: colorBack[selected?.value] },
+                      }
+                    },
+                    menuPortal: (base) => ({ ...base, zIndex: 9999 }),
+                    menu: (baseStyles) => ({
+                      ...baseStyles,
+                      borderRadius: "10px",
+                      display: "flex",
+                      width: "max-content",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      backgroundColor: "#f2f2f2",
+                      fontWeight: 600,
+                    }),
+                    option: (baseStyles, state) => {
+                      const color = colorMap[state.data.value]
+                      return {
+                        ...baseStyles,
+                        backgroundColor: "transparent",
+                        color,
+                      };
+                    },
+                    indicatorSeparator: (styles) => ({ ...styles, display: "none" }),
+                    dropdownIndicator: (styles) => ({ ...styles, display: "none" }),
+                    valueContainer: (baseStyles) => ({ ...baseStyles, padding: "0", margin: "0 8px" }),
+                    singleValue: (baseStyles, state) => {
+                      const color = colorMap[state.data.value]
+                      return {
+                        ...baseStyles,
+                        color,
+                        paddingLeft: "0",
+                      };
+                    },
 
-              }}
-              isSearchable={false}
-              closeMenuOnScroll={true}
-              menuPortalTarget={document.body}
-              menuPlacement="auto"
-              menuPosition="fixed"
-              onChange={(e) => {
-                if (!e) return;
-                setSelected(e)
-                updateState.mutate(e.value);
-              }}
-            />
+                  }}
+                  isSearchable={false}
+                  closeMenuOnScroll={true}
+                  menuPortalTarget={document.body}
+                  menuPlacement="auto"
+                  menuPosition="fixed"
+                  onChange={(e) => {
+                    if (!e) return;
+                    setSelected(e)
+                    updateState.mutate(e.value);
+                  }}
+                />
+              </div>
+              <div className="order_body">
+
+                <div className="order_detail_item">
+                  <span>Origen</span>
+                  {data.origin}</div>
+                <div className="order_detail_item">
+                  <span>Destino</span>
+                  {data.destination}</div>
+                <div className="order_detail_item">
+                  <span>Tipo de viaje</span>
+                  {data.trip_type === 1 ? "One way" : "Round trip"}</div>
+                <div className="order_detail_item">
+                  <span>Fecha y hora</span>
+                  {fecha(data.departureDate)} - {formatHour(data.departureHours)}</div>
+                <div className="order_detail_item">
+                  <span>Total</span>
+                  {moneyFormant(data.total)}</div>
+              </div>
+            </div>
+            <div className="right">
+              <div className="order_header">
+                <h3>Cliente</h3>
+              </div>
+              <div className="order_body">
+                <div className="order_detail_item">
+                  <span>Nombre</span>
+                  {data.customer.name}</div>
+                <div className="order_detail_item">
+                  <span>Apellido</span>
+                  {data.customer.lastName}</div>
+                <div className="order_detail_item">
+                  <span>Email</span>
+                  {data.customer.email}</div>
+                <div className="order_detail_item">
+                  <span>Phone</span>
+                  {data.customer.phone}</div>
+              </div>
+            </div>
           </div>
-          <div>{data.origin}</div>
-          <div>{data.destination}</div>
-          <div>{data.trip_type === 1 ? "One way" : "Round trip"}</div>
-          <div>{fecha(data.departureDate)}</div>
-          <div>{data.departureHours}</div>
-          <div>{data.customer.name}</div>
-          <div>{data.customer.lastName}</div>
-          <div>{moneyFormant(data.total)}</div>
         </div>
       );
     }
@@ -187,7 +252,7 @@ const OrderDetail = () => {
     }
   }, [isLoading, data])
   return <div>
-    <Toaster/>
+    <Toaster />
     {content()}</div>;
 };
 
