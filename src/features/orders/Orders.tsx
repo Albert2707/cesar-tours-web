97b3987f5a0e8ec26709fe35e75b94d46f1d1fb3
Albert2707 <albertjohan2707@gmail.com>
2024-12-19 09:49:39 -0400

    feat: new filter by order num

--- src/features/orders/Orders.tsx
+++ src/features/orders/Orders.tsx
@@ -4,7 +4,7 @@ import { moneyFormant } from "@/utils/functions/moneyFormat";
 import { format } from "date-fns";
 import "./Order.scss";
 import { es } from "date-fns/locale";
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import Button from "@/shared/components/button/Button";
 import { useNavigate } from "react-router-dom";
 import useTranslate from "@hooks/translations/Translate";
@@ -14,10 +14,16 @@ const Orders = () => {
   const navigate = useNavigate();
   const { translate } = useTranslate();
   const [skip, setSkip] = useState<number>(1);
+  const [reservation_num, setReservation_num] = useState<string>("");
   const [hasNextPage, setHasNextPage] = useState<boolean>(true);
   const limit: number = 5;
   const { data, isLoading, error, refetch } = useQuery(`orders`, async () => {
-    const res = await OrderServices.getOrders(filter, skip, limit);
+    const res = await OrderServices.getOrders(
+      filter,
+      skip,
+      limit,
+      reservation_num
+    );
     setHasNextPage(res.hasNextPage);
     setPageCount(res.totalPages);
     return res.order;
@@ -81,74 +87,116 @@ const Orders = () => {
   useEffect(() => {
     refetch();
   }, [filter, skip]);
+
+  useEffect(() => {
+    if (reservation_num === "") refetch();
+  }, [reservation_num, refetch]);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    refetch();
+  };
   return (
     <div className="orders">
       <div className="filter">
-        <Button
-          properties={{
-            type: "filter",
-            onClickfn: () => {
-              setFilter("all");
-              setSkip(1);
-            },
-          }}
-        >
-          {translate("all")}
-        </Button>
-        <Button
-          properties={{
-            type: "filter",
-            onClickfn: () => {
-              setFilter("0");
-              setSkip(1);
-            },
-          }}
-        >
-          {translate("scheduled")}
-        </Button>
-        <Button
-          properties={{
-            type: "filter",
-            onClickfn: () => {
-              setFilter("1");
-              setSkip(1);
-            },
-          }}
-        >
-          {translate("in_progress")}
-        </Button>
-        <Button
-          properties={{
-            type: "filter",
-            onClickfn: () => {
-              setFilter("2");
-              setSkip(1);
-            },
-          }}
-        >
-          {translate("completed")}
-        </Button>
-        <Button
-          properties={{
-            type: "filter",
-            onClickfn: () => {
-              setFilter("3");
-              setSkip(1);
-            },
-          }}
-        >
-          {translate("canceled")}
-        </Button>
+        <div className="button_filter">
+          <Button
+            properties={{
+              type: "filter",
+              btnClass: `${filter == "all" ? "selected" : ""}`,
+              onClickfn: () => {
+                setFilter("all");
+                setSkip(1);
+              },
+            }}
+          >
+            {translate("all")}
+          </Button>
+          <Button
+            properties={{
+              type: "filter",
+              btnClass: `${filter == "0" ? "selected" : ""}`,
+              onClickfn: () => {
+                setFilter("0");
+                setSkip(1);
+              },
+            }}
+          >
+            {translate("scheduled")}
+          </Button>
+          <Button
+            properties={{
+              type: "filter",
+              btnClass: `${filter == "1" ? "selected" : ""}`,
+              onClickfn: () => {
+                setFilter("1");
+                setSkip(1);
+              },
+            }}
+          >
+            {translate("in_progress")}
+          </Button>
+          <Button
+            properties={{
+              type: "filter",
+              btnClass: `${filter == "2" ? "selected" : ""}`,
+              onClickfn: () => {
+                setFilter("2");
+                setSkip(1);
+              },
+            }}
+          >
+            {translate("completed")}
+          </Button>
+          <Button
+            properties={{
+              type: "filter",
+              btnClass: `${filter == "3" ? "selected" : ""}`,
+              onClickfn: () => {
+                setFilter("3");
+                setSkip(1);
+              },
+            }}
+          >
+            {translate("canceled")}
+          </Button>
+        </div>
+        <form className="order_search" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder={translate("reservationNumber")}
+            onChange={(e) => {
+              setReservation_num(e.target.value);
+            }}
+          />
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-search"
+            >
+              <circle cx="11" cy="11" r="8" />
+              <path d="m21 21-4.3-4.3" />
+            </svg>
+          </button>
+        </form>
       </div>
       <div className="table">
         <div className="tbl-header">
-          <div className="cell">{translate('order_number')}</div>
-          <div className="cell">{translate('date')}</div>
-          <div className="cell">{translate('status')}</div>
-          <div className="cell">{translate('customer')}</div>
-          <div className="cell">{translate('origin2')}</div>
-          <div className="cell">{translate('vehicle')}</div>
-          <div className="cell">{translate('total')}</div>
+          <div className="cell">{translate("order_number")}</div>
+          <div className="cell">{translate("date")}</div>
+          <div className="cell">{translate("status")}</div>
+          <div className="cell">{translate("customer")}</div>
+          <div className="cell">{translate("origin2")}</div>
+          <div className="cell">{translate("vehicle")}</div>
+          <div className="cell">{translate("total")}</div>
         </div>
         <div className="tbl-body">{order()}</div>
       </div>
