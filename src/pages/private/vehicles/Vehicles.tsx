efd4156fbfb52c0f03999d0661e8c7b172be7002
Albert2707 <albertjohan2707@gmail.com>
2024-12-19 09:50:24 -0400

    feat: warn message in mobile mode

--- src/pages/private/vehicles/Vehicles.tsx
+++ src/pages/private/vehicles/Vehicles.tsx
@@ -144,6 +144,27 @@ const Vehicles = () => {
         )}
       </AnimatePresence>
       <div className="filters">
+      <div className="warn_mobile">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-triangle-alert"
+            >
+              <path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3" />
+              <path d="M12 9v4" />
+              <path d="M12 17h.01" />
+            </svg>
+            <span className="">
+              {translate("only_remove_unoccupied_vehicles")}
+            </span>
+          </div>
         <div className="filter">
           <Button properties={{ type: "filter", onClickfn: () => {} }}>
             {translate("available")}
