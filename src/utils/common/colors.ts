83229bc318751c0c507609fe9836096940aefeef
Albert <albertjohan2707@gmail.com>
2024-12-20 13:16:06 -0400

    feat: common colors

---
+++ src/utils/common/colors.ts
@@ -0,0 +1,12 @@
+export const colorMap: { [key: number]: string } = {
+  0: "#d97706", // Programada (Azul)
+  1: "#f59e0b", // En Curso (Amarillo)
+  2: "#47a967", // Finalizada (Verde)
+  3: "#dc3545", // Cancelada (Rojo)
+};
+export const colorBack: { [key: number]: string } = {
+  0: "#faefe6",
+  1: "#fff3cd",
+  2: "#eef7f1",
+  3: "#f8d7da",
+};
