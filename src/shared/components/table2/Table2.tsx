d874eb1b831043d999651f5a3d056bc4def760fa
Albert <albertjohan2707@gmail.com>
2024-12-20 13:15:52 -0400

    feat: interfaces

--- src/shared/components/table2/Table2.tsx
+++ src/shared/components/table2/Table2.tsx
@@ -1,5 +1,4 @@
 import { format } from "date-fns";
-import { FC } from "react";
 import Button from "@/shared/components/button/Button";
 import { moneyFormant } from "@/utils/functions/moneyFormat";
 import { useNavigate } from "react-router-dom";
@@ -8,71 +7,21 @@ import useTranslate from "@hooks/translations/Translate";
 import "./Table2.scss";
 import { VITE_CESAR_API } from "@/config/config";
 import { useVehicleStore } from "@hooks/vehicles/useVehicleStore";
-export interface ICountry {
-  country_id: string;
-  country: string;
-}
-export interface IVehicle {
-  id: string;
-  brand: string;
-  model: string;
-  capacity: number;
-  luggage_capacity: number;
-  price_per_km: number;
-  img_url: string;
-  status: boolean;
-}
-
-export interface ICustomer {
-  customer_id: string;
-  name: string;
-  lastName: string;
-  email: string;
-  phone: string;
-  optionalPhone: string;
-  country: ICountry;
-  countryId: string;
-}
-
-export interface IOrder {
-  order_num: string;
-  origin: string;
-  destination: string;
-  trip_type: "one_way" | "round_trip";
-  passengers: number;
-  luggage: number;
-  departureDate: string;
-  departureHours: string;
-  returnDate?: string | null;
-  returnHours?: string | null;
-  country: ICountry;
-  countryId: string;
-  distance: string;
-  duration: string;
-  vehicle: IVehicle;
-  vehicleId: string;
-  status: number;
-  customer: ICustomer;
-  customerId: string;
-  airline: string;
-  flight_number: string;
-  additionalNotes?: string | null;
-  paymentMethod: "Cash" | "Card";
-  total: number;
-}
+import { VehicleModel } from "@/models/booking/vehicle";
+import { IOrder } from "@/shared/interfaces/interfaces";
 
 interface Header {
   column: string;
   key: string;
 }
-interface Props {
-  data: IOrder[];
+interface Props<T> {
+  data: T[];
   headers: Header[];
   isOrder: boolean;
 }
 
-const Table2: FC<Props> = ({ data, headers, isOrder }) => {
-  const { setConfirm, setVehicle, setEditMode, setShow, setVehicleId } =
+const Table2 = <T extends IOrder | VehicleModel>({ data, headers, isOrder }: Props<T>) => {
+    const { setConfirm, setVehicle, setEditMode, setShow, setVehicleId } =
     useVehicleStore();
   const orderStatus = (
     status: number | boolean
@@ -94,12 +43,10 @@ const Table2: FC<Props> = ({ data, headers, isOrder }) => {
   const { translate } = useTranslate();
 
   const navigate = useNavigate();
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const renderCell = (header: { key: string }, order: any) => {
+    const value = header.key.split(".").reduce((acc, key) => acc[key], order); // Acceso dinámico a propiedades anidadas
 
-  const renderCell = (header:{key:string}, e) => {
-    const value = header.key
-      .split(".")
-      .reduce((acc, key) => acc[key], e); // Acceso dinámico a propiedades anidadas
-  
     switch (header.key) {
       case "departureDate":
         return format(new Date(value), "MMM d, yyyy", { locale: es });
@@ -123,8 +70,8 @@ const Table2: FC<Props> = ({ data, headers, isOrder }) => {
                 width: "100%",
                 objectFit: "cover",
               }}
-              src={VITE_CESAR_API + "/" + e.img_url}
-              alt={e.brand}
+              src={VITE_CESAR_API + "/" + order.img_url}
+              alt={order.brand}
             />
           </div>
         );
@@ -132,9 +79,8 @@ const Table2: FC<Props> = ({ data, headers, isOrder }) => {
         return value;
     }
   };
-  
 
-  const renderActions = (e) => {
+  const renderActions = (e: IOrder | VehicleModel) => {
     if (isOrder) {
       return (
         <div className="cell">
@@ -142,7 +88,7 @@ const Table2: FC<Props> = ({ data, headers, isOrder }) => {
             properties={{
               type: "primary",
               onClickfn: () => {
-                navigate(`/admin/order-detail/${e.order_num}`);
+                navigate(`/admin/order-detail/${(e as IOrder).order_num}`);
               },
             }}
           >
@@ -151,14 +97,14 @@ const Table2: FC<Props> = ({ data, headers, isOrder }) => {
         </div>
       );
     }
-  
+
     return (
       <div className="options" style={{ display: "flex", gap: "5px" }}>
         <Button
           properties={{
             type: "options",
             onClickfn: () => {
-              setVehicle(e);
+              setVehicle(e as VehicleModel);
               setEditMode(true);
               setShow(true);
             },
@@ -185,7 +131,7 @@ const Table2: FC<Props> = ({ data, headers, isOrder }) => {
             properties={{
               type: "options",
               onClickfn: () => {
-                setVehicleId(e.id);
+                setVehicleId((e as VehicleModel).id);
                 setConfirm(true);
               },
             }}
@@ -212,25 +158,22 @@ const Table2: FC<Props> = ({ data, headers, isOrder }) => {
       </div>
     );
   };
-  
 
   const order = () => {
     if (!data || data.length === 0) {
       return <div>No hay registros en la tabla</div>;
     }
-  
-    return data.map((e: any) => (
-      <div className="tbl-body-row" key={crypto.randomUUID()}>
+
+    return data.map((e, index) => (
+      <div className="tbl-body-row" key={index}>
         {headers
           .filter((header) => header.column)
-          .map((header, colIndex) => (
+          .map((header) => (
             <div
               className={`cell ${
-                header.key === "status" &&
-                isOrder &&
-                orderStatus(e.status).class
+                header.key === "status" && isOrder && orderStatus((e as IOrder).status).class
               }`}
-              key={colIndex}
+              key={header.key}
             >
               {renderCell(header, e)}
             </div>
@@ -239,163 +182,6 @@ const Table2: FC<Props> = ({ data, headers, isOrder }) => {
       </div>
     ));
   };
-  
-
-  // const order = () => {
-  //   if (!data || data.length === 0) {
-  //     return <div>No hay registros en la tabla</div>;
-  //   } else {
-  //     return data.map((e: any) => (
-  //       <div className="tbl-body-row" key={crypto.randomUUID()}>
-  //         {headers
-  //           .filter((e) => e.column)
-  //           .map((header, colIndex) => {
-  //             const value = header.key
-  //               .split(".")
-  //               .reduce((acc, key) => acc[key], e); // Acceso dinámico a propiedades anidadas
-  //             return (
-  //               <div
-  //                 className={`cell ${
-  //                   header.key === "status" &&
-  //                   isOrder === true &&
-  //                   orderStatus(e.status).class
-  //                 }`}
-  //                 key={colIndex}
-  //               >
-  //                 {header.key === "departureDate" ? (
-  //                   format(new Date(value), "MMM d, yyyy", { locale: es })
-  //                 ) : header.key === "status" ? (
-  //                   orderStatus(value).name
-  //                 ) : header.key === "total" ? (
-  //                   moneyFormant(value)
-  //                 ) : header.key == "img_url" ? (
-  //                   <div
-  //                     className="avehicle_img"
-  //                     style={{
-  //                       width: "200px",
-  //                       height: "150px",
-  //                       overflow: "hidden",
-  //                     }}
-  //                   >
-  //                     <img
-  //                       style={{
-  //                         height: "100%",
-  //                         width: "100%",
-  //                         objectFit: "cover",
-  //                       }}
-  //                       src={VITE_CESAR_API + "/" + e.img_url}
-  //                       alt={e.brand}
-  //                     />
-  //                   </div>
-  //                 ) : (
-  //                   value
-  //                 )}
-  //               </div>
-  //             );
-  //           })}
-
-  //         {isOrder ? (
-  //           <div className="cell">
-  //             <Button
-  //               properties={{
-  //                 type: "primary",
-  //                 onClickfn: () => {
-  //                   navigate(`/admin/order-detail/${e.order_num}`);
-  //                 },
-  //               }}
-  //             >
-  //               {translate("details")}
-  //             </Button>
-  //           </div>
-  //         ) : (
-  //           <div className="options" style={{ display: "flex", gap: "5px" }}>
-  //             <Button
-  //               properties={{
-  //                 type: "options",
-  //                 onClickfn: () => {
-  //                   setVehicle(e);
-  //                   setEditMode(true);
-  //                   setShow(true);
-  //                 },
-  //               }}
-  //             >
-  //               <svg
-  //                 xmlns="http://www.w3.org/2000/svg"
-  //                 width="24"
-  //                 height="24"
-  //                 viewBox="0 0 24 24"
-  //                 fill="none"
-  //                 stroke="currentColor"
-  //                 strokeWidth="2"
-  //                 strokeLinecap="round"
-  //                 strokeLinejoin="round"
-  //                 className="btn_icon"
-  //               >
-  //                 <path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
-  //               </svg>
-  //               {translate("edit")}
-  //             </Button>
-  //             {e.status && (
-  //               <Button
-  //                 properties={{
-  //                   type: "options",
-  //                   onClickfn: () => {
-  //                     setVehicleId(e.id);
-  //                     setConfirm(true);
-  //                   },
-  //                 }}
-  //               >
-  //                 <svg
-  //                   xmlns="http://www.w3.org/2000/svg"
-  //                   width="24"
-  //                   height="24"
-  //                   viewBox="0 0 24 24"
-  //                   fill="none"
-  //                   stroke="currentColor"
-  //                   strokeWidth="2"
-  //                   strokeLinecap="round"
-  //                   strokeLinejoin="round"
-  //                   className="btn_icon"
-  //                 >
-  //                   <path d="M3 6h18" />
-  //                   <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
-  //                   <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
-  //                 </svg>
-  //                 {translate("delete")}
-  //               </Button>
-  //             )}
-  //           </div>
-  //         )}
-  //         {/* <div className="cell">{e.order_num}</div>
-  //         <div className="cell">
-  //           {format(new Date(e.departureDate), "MMM d, yyyy", { locale: es })}
-  //         </div>
-
-  //         <div className={`cell ${orderStatus(e.status).class}`}>
-  //           {orderStatus(e.status).name}
-  //         </div>
-  //         <div className="cell">{`${e.customer.name}`}</div>
-
-  //         <div className="cell">{e.origin}</div>
-  //         <div className="cell">{e.vehicle.model}</div>
-
-  //         <div className="cell">{moneyFormant(e.total)}</div>
-  //         <div className="cell">
-  //           <Button
-  //             properties={{
-  //               type: "primary",
-  //               onClickfn: () => {
-  //                 navigate(`/admin/order-detail/${e.order_num}`);
-  //               },
-  //             }}
-  //           >
-  //             {translate("details")}
-  //           </Button>
-  //         </div> */}
-  //       </div>
-  //     ));
-  //   }
-  // };
 
   return (
     <div className="table">
---
+++ src/shared/interfaces/interfaces.ts
@@ -0,0 +1,54 @@
+import { VehicleModel } from "@/models/booking/vehicle";
+
+export interface ICountry {
+  country_id: string;
+  country: string;
+}
+export interface ICustomer {
+  customer_id: string;
+  name: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  optionalPhone: string;
+  country: ICountry;
+  countryId: string;
+}
+
+export interface IOrder {
+  order_num: string;
+  origin: string;
+  destination: string;
+  trip_type: "one_way" | "round_trip";
+  passengers: number;
+  luggage: number;
+  departureDate: string;
+  departureHours: string;
+  returnDate?: string | null;
+  returnHours?: string | null;
+  country: ICountry;
+  countryId: string;
+  distance: string;
+  duration: string;
+  vehicle: VehicleModel;
+  vehicleId: string;
+  status: number;
+  customer: ICustomer;
+  customerId: string;
+  airline: string;
+  flight_number: string;
+  additionalNotes?: string | null;
+  paymentMethod: "Cash" | "Card";
+  total: number;
+}
+
+export interface Translations {
+  [key: string]: string | string[];
+}
+
+export interface TableParameters {
+  accesor: string;
+  total: string;
+  key: keyof TableParameters;
+  value: keyof TableParameters;
+}
