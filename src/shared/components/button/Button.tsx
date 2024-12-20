0bfa523674065f1ac8531074a2ef1b387fdfbb3a
Albert <albertjohan2707@gmail.com>
2024-12-20 17:59:42 -0400

    refactor: format code

--- src/shared/components/button/Button.tsx
+++ src/shared/components/button/Button.tsx
@@ -1,23 +1,39 @@
-import { FC } from 'react'
-import "./Button.scss"
+import { CSSProperties, FC } from "react";
+import "./Button.scss";
 interface ButtonProps {
-    // text: string,
-    type: "primary" | "secondary" | "logout" | "filter" | "options" | "toast" | "back"
-    |"back-login"
-    onClickfn: () => void
-    disabled?: boolean
-    btnClass?: string
+  // text: string,
+  type:
+    | "primary"
+    | "secondary"
+    | "logout"
+    | "filter"
+    | "options"
+    | "toast"
+    | "back"
+    | "back-login";
+  onClickfn: () => void;
+  disabled?: boolean;
+  btnClass?: string;
+  style?: CSSProperties;
 }
 interface Props {
-    properties: ButtonProps
-    children: React.ReactNode
+  properties: ButtonProps;
+  children: React.ReactNode;
 }
-const Button: FC<Props> = ({ children, properties: { type, onClickfn, disabled, btnClass } }) => {
-    return (
-        <button className={`btn-${type} ${btnClass}`} onClick={onClickfn} disabled={disabled}>
-            {children}
-        </button>
-    )
-}
+const Button: FC<Props> = ({
+  children,
+  properties: { type, onClickfn, disabled, btnClass, style },
+}) => {
+  return (
+    <button
+      className={`btn-${type} ${btnClass}`}
+      onClick={onClickfn}
+      disabled={disabled}
+      style={style}
+    >
+      {children}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
