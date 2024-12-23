4ec675745128576139365e855c79f5018dfe9b76
Albert <albertjohan2707@gmail.com>
2024-12-23 16:37:34 -0400

    feat: method to delete an order

--- src/shared/components/table2/Table2.tsx
+++ src/shared/components/table2/Table2.tsx
@@ -25,7 +25,7 @@ const Table2 = <T extends IOrder | VehicleModel>({
   headers,
   isOrder,
 }: Props<T>) => {
-  const { setConfirm, setVehicle, setEditMode, setShow, setVehicleId } =
+  const { setConfirm, setVehicle, setEditMode, setShow, setVehicleId,setOrderId} =
     useVehicleStore();
   const getOrderStatus = (status: number): { name: string; class: string } => {
     switch (status) {
@@ -88,6 +88,7 @@ const Table2 = <T extends IOrder | VehicleModel>({
               width: "100%",
               height: "100%",
               overflow: "hidden",
+              borderRadius: "10px",
             }}
           >
             <img
@@ -109,17 +110,46 @@ const Table2 = <T extends IOrder | VehicleModel>({
   const renderActions = (e: IOrder | VehicleModel) => {
     if (isOrder) {
       return (
-        <div className="cell">
+        <div className="cell" style={{ display: "flex", gap: "5px" }}>
           <Button
             properties={{
-              type: "primary",
+              type: "options",
               style: { alignSelf: "center" },
               onClickfn: () => {
                 navigate(`/admin/order-detail/${(e as IOrder).order_num}`);
               },
             }}
           >
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-info"><circle cx="12" cy="12" r="10" /><path d="M12 16v-4" /><path d="M12 8h.01" /></svg>
             {translate("details")}
+          </Button>
+          <Button
+            properties={{
+              type: "options",
+              style: { alignSelf: "center" },
+              onClickfn: () => {
+                setOrderId((e as IOrder).order_num);
+                setConfirm(true);
+              },
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="btn_icon"
+            >
+              <path d="M3 6h18" />
+              <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+              <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+            </svg>
+            {translate("delete")}
           </Button>
         </div>
       );
@@ -208,11 +238,10 @@ const Table2 = <T extends IOrder | VehicleModel>({
           .filter((header) => header.column)
           .map((header) => (
             <div
-              className={`cell ${
-                header.key === "status" &&
+              className={`cell ${header.key === "status" &&
                 isOrder &&
                 orderStatus((e as IOrder).status, isOrder).class
-              }`}
+                }`}
               key={header.key}
             >
               {renderCell(header, e)}
