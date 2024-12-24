9fb26f89398633d12e0d42697df73de7cb7fe04b
Albert <albertjohan2707@gmail.com>
2024-12-24 00:50:28 -0400

    fix: using IOrder instead Order interface

--- src/shared/hooks/confirmation/useConfirmationStore.ts
+++ src/shared/hooks/confirmation/useConfirmationStore.ts
@@ -1,37 +1,37 @@
-import { Location } from "@hooks/booking/useBookingStore";
+import { IOrder } from "@/shared/interfaces/interfaces";
 import { create } from "zustand";
 
-type Order = {
-  order_num: string;
-  origin: Location;
-  destination: Location;
-  trip_type: "one_way" | "round_trip";
-  passengers: number;
-  luggage: number;
-  departureDate: Date;
-  departureHours: string;
-  countryId: string;
-  distance: string;
-  duration: string;
-  vehicleId: string;
-  customerId: string;
-  airline: string;
-  flight_number: string;
-  additionalNotes: string;
-  paymentMethod: "Card" | "Cash" | "Other";
-  total: number;
-  returnDate: Date | null;
-  returnHours: string | null;
-  status: number;
-  createAt: Date;
-  updateAt: Date;
-};
+// type Order = {
+//   order_num: string;
+//   origin: Location;
+//   destination: Location;
+//   trip_type: "one_way" | "round_trip";
+//   passengers: number;
+//   luggage: number;
+//   departureDate: Date;
+//   departureHours: string;
+//   countryId: string;
+//   distance: string;
+//   duration: string;
+//   vehicleId: string;
+//   customerId: string;
+//   airline: string;
+//   flight_number: string;
+//   additionalNotes: string;
+//   paymentMethod: "Card" | "Cash" | "Other";
+//   total: number;
+//   returnDate: Date | null;
+//   returnHours: string | null;
+//   status: number;
+//   createAt: Date;
+//   updateAt: Date;
+// };
 type Confirmation = {
-  order?: Order;
+  order?: IOrder;
 };
 
 export type actions = {
-  addOrder(order: Order): void;
+  addOrder(order: IOrder): void;
 };
 export const useConfirmationStore = create<Confirmation & actions>()((set) => ({
   addOrder: (order) => set({ order }),
